21c01968f904b50a923e305fddc1303cb690ed16
Mutombe <simbarashemutombe1@gmail.com>
2025-05-03 07:26:49 +0200

    font

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,10 +4,6 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-rg5$^x0nm)lmj0v6-2^x#nhk9uewd&k=lc@%jsu3^f^y3##34)"
 
