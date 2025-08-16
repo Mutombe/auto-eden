3640a6389a91c1f48c121929ad9407ba03b91821
Mutombe <simbarashemutombe1@gmail.com>
2025-08-16 22:45:41 +0200

    correct backend url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -22,7 +22,7 @@ export const refreshTokens = async (refresh) => {
 };
 
 const api = axios.create({
-  baseURL:  'http://127.0.0.1:8000/',
+  baseURL:  'http://auto-eden-backend.onrender.com/',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
