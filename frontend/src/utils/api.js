a8c25cc6a03282d90921c8950b59cdd3a175e566
Mutombe <simbarashemutombe1@gmail.com>
2025-08-18 11:46:36 +0200

    ///

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -22,7 +22,7 @@ export const refreshTokens = async (refresh) => {
 };
 
 const api = axios.create({
-  baseURL: "http://auto-eden-backend.onrender.com/",
+  baseURL: "http://auto-eden-backend.onrender.com",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
