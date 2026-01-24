5998c5f8a03cf55119beb205d4fbf4a0cf9461b2
Mutombe <simbarashemutombe1@gmail.com>
2026-01-24 16:58:18 +0200

    ....

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -58,69 +58,6 @@ const AuthSetup = () => {
 function App() {
   return (
     <div className="pacaembu-font">
-                <style jsx>{`
-            /* Pacaembu Font Faces */
-            @font-face {
-              font-family: "Pacaembu";
-              src: url("./fonts/Pacaembu-Thin-Trial.ttf") format("truetype");
-              font-weight: 100;
-              font-style: normal;
-              font-display: swap;
-            }
-
-            @font-face {
-              font-family: "Pacaembu";
-              src: url("./fonts/Pacaembu-Light-Trial.ttf") format("truetype");
-              font-weight: 300;
-              font-style: normal;
-              font-display: swap;
-            }
-
-            @font-face {
-              font-family: "Pacaembu";
-              src: url("./fonts/Pacaembu-Regular-Trial.ttf") format("truetype");
-              font-weight: 400;
-              font-style: normal;
-              font-display: swap;
-            }
-
-            @font-face {
-              font-family: "Pacaembu";
-              src: url("./fonts/Pacaembu-Medium-Trial.ttf") format("truetype");
-              font-weight: 500;
-              font-style: normal;
-              font-display: swap;
-            }
-
-            @font-face {
-              font-family: "Pacaembu";
-              src: url("./fonts/Pacaembu-Bold-Trial.ttf") format("truetype");
-              font-weight: 700;
-              font-style: normal;
-              font-display: swap;
-            }
-
-            @font-face {
-              font-family: "Pacaembu";
-              src: url("./fonts/Pacaembu-Black-Trial.ttf") format("truetype");
-              font-weight: 900;
-              font-style: normal;
-              font-display: swap;
-            }
-
-            @font-face {
-              font-family: "Pacaembu";
-              src: url("./fonts/Pacaembu-Ultra-Trial.ttf") format("truetype");
-              font-weight: 950;
-              font-style: normal;
-              font-display: swap;
-            }
-
-            .pacaembu-font {
-              font-family: "Pacaembu", "Inter", "Segoe UI", Tahoma, Geneva,
-                Verdana, sans-serif;
-            }
-          `}</style>
     <>
       <ToastContainer
         position="top-right"
